f69f9bf0907436221d4a549f2b65aa90bafe34cd
gtapiero <giovanny.tapiero@pi.edu.co>
2025-06-11 13:18:11 -0500

    Agregar archivos de proyecto.

---
+++ ZapatosAlmacenApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using ZapatosAlmacenApp.Models;
+
+namespace ZapatosAlmacenApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+      public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+      : base(options) { }
+
+      public DbSet<Zapato> Zapatos { get; set; }
+    }
+}
---
+++ ZapatosAlmacenApp/Models/Zapato.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ZapatosAlmacenApp.Models
+{
+    public class Zapato
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Marca { get; set; }
+
+        [Required]
+        public string Modelo { get; set; }
+
+        [Range(20, 50)]
+        public int Talla { get; set; }
+
+        public string Color { get; set; }
+
+        [Range(0, int.MaxValue)]
+        public int Cantidad { get; set; }
+
+        [Range(0, double.MaxValue)]
+        public decimal Precio { get; set; }
+    }
+}
